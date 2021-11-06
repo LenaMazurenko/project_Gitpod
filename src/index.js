c965887785f1ef758ed676b59d7b4d3e4f1a1c3f
LenaMazurenko <lena@rent-it.su>
2021-11-06 20:19:45 +0200

    add header styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+const refs = {
+  mobMenuBtn: document.querySelector('.header__burger'),
+  dropMenu: document.querySelector('.header__menu'),
+  body: document.querySelector('body'),
+};
+
+refs.mobMenuBtn.addEventListener('click', () => {
+  refs.mobMenuBtn.classList.toggle('active');
+  refs.dropMenu.classList.toggle('active');
+  refs.body.classList.toggle('lock');
+});
