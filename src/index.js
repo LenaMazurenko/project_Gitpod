6d19c330752dc9a8b6ff3d41f6532a03580524ae
LenaMazurenko <lena@rent-it.su>
2021-11-17 19:23:14 +0200

    add popup 'Login'

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const refs = {
   headerList: document.querySelector('.header__list'),
   body: document.querySelector('body'),
   header: document.querySelector('.header'),
+
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+
+  modalLogin: document.querySelector('.modal-login'),
+  linkToSignup: document.getElementById('to_signup'),
 };
 
 /*---add/remove class for mobile menu-----*/
@@ -27,4 +34,19 @@ window.addEventListener('scroll', function (e) {
   let last_known_scroll_position = window.scrollY;
   if (last_known_scroll_position > 0) refs.header.classList.add('scroll');
   else refs.header.classList.remove('scroll');
+});
+
+/*----------open modal LogIN------------*/
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+  refs.modalLogin.querySelector('.modal-login__form').classList.add('active');
+  refs.modalLogin.querySelector('.modal-signup__form').classList.remove('active');
+}
+refs.linkToSignup.addEventListener('click', () => {
+  refs.modalLogin.querySelector('.modal-login__form').classList.remove('active');
+  refs.modalLogin.querySelector('.modal-signup__form').classList.add('active');
 });
